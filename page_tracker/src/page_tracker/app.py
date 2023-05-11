cf2c542a574126d3e2a1d82d16a7c1e1a48e94fe
Anton Zotov <zotoff@gmail.com>
2023-05-11 15:37:32 -0400

    Initial commit of a working app/before containerization

---
+++ page_tracker/src/page_tracker/app.py
@@ -0,0 +1,27 @@
+# src/page_tracker/app.py
+
+import os
+from functools import cache
+
+from flask import Flask
+from redis import Redis, RedisError
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{pensive face}", 500
+    else:
+        return f"This page has been seen {page_views} times."
+
+
+@cache
+def redis():
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
+
